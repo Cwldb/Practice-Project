a97771a84867aa83266a30b7fad56faaf2d48a92
Cwldb <mozi0810@gmail.com>
2025-03-09 20:43:27 +0900

    feat : PlayerInputSO

---
+++ 2_Practice_project/Assets/01_Scripts/Player/Movement/PlayerInputSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerInput", menuName = "SO/PlayerInput", order = 0)]
+public class PlayerInputSO : ScriptableObject//, Controls.IPlayerActions
+{
+    
+}
