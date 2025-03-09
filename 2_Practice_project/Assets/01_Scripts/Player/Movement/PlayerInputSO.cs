37fdd2a7cdab229d4c544d0f9e973fa15433d1c9
Cwldb <mozi0810@gmail.com>
2025-03-09 21:20:34 +0900

    fix : PlayerInput

--- 2_Practice_project/Assets/01_Scripts/Player/Movement/PlayerInputSO.cs
+++ 2_Practice_project/Assets/01_Scripts/Player/Movement/PlayerInputSO.cs
@@ -1,7 +1,11 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerInput", menuName = "SO/PlayerInput", order = 0)]
 public class PlayerInputSO : ScriptableObject//, Controls.IPlayerActions
 {
-    
+    public event Action<Vector2> OnMovementChange;
+    public event Action OnAttackPressed;
+
+
 }
